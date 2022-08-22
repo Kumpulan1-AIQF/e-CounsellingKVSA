2adb6ce87937cdd7c87fcd01d248082dd864f593
iqbaalrith <67789967+iqbaalrith56@users.noreply.github.com>
2022-08-22 09:06:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active"><a class="nav-link" href="#home">Home |<span class="sr-only">(current)</span></a></li>
+                        <li class="nav-item active"><a class="nav-link" href="#home">Home |</a></li>
                         <li class="nav-item"><a class="nav-link" href="login.php">Session |</a></li>
                         <li class="nav-item"><a class="nav-link" href="#event">Event |</a></li>
                         <li class="nav-item"><a class="nav-link" href="#service">Service |</a></li>
@@ -130,7 +130,7 @@
     </section>
 
     <!-- Service -->
-    <section class="py-5 section-1" id="service">
+    <section class="py-5 section-3" id="service">
 
         <div class="service">
             <h2 style="text-align: center;">Services</h2>
@@ -171,7 +171,7 @@
     </section>
 
     <!-- About Us -->
-    <section class="py-5 section-2" id="aboutus">
+    <section class="py-5 section-4" id="aboutus">
         <div class="container py-5">
             <div class="row">
                 <div class="col-md-6">
@@ -187,7 +187,7 @@
     </section>
 
     <!-- Contact Us -->
-    <section class="py-5 section-1" id="contactus">
+    <section class="py-5 section-5" id="contactus">
         <div class="container py-5 text-center">
             <div class="row">
                 <div class="col-lg-8 mx-auto">
