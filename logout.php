1cc8a98ddb09e151dc837ad957f6e77f7afe9de8
Abdullahamien <56059664+Abdullahamien@users.noreply.github.com>
2022-08-28 15:29:29 +0800

    Update and rename logout.html to logout.php

--- logout.php
+++ logout.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+
+?>
+
 <html>
 
 <head>
@@ -63,12 +70,12 @@
                     aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="#home">Home |</a></li>
-                        <li class="nav-item"><a class="nav-link" href="EDIT HERE">Session |</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#event">Event |</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#service">Service |</a></li>
-                        <li class="nav-item"><a class="nav-link" href="aboutus.html">About us |</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#contactus">Contact us |</a></li>
+                         <li class="nav-item active"><a class="nav-link" href="index.php">Home |<span class="sr-only">(current)</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Session |</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Event |</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Service |</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">About us |</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Contact us |</a></li>
                     </ul>
                 </div>
             </div>
@@ -80,7 +87,7 @@
             <h4>Thank you!</h4>
             <h5>You are now logged out</h5>
             <br>
-            <a href="index.php" class="btn btn-info">Log Back In</a>
+            <a href="index.php" class="btn btn-info">Main Page</a>
         </div>
     </center>
 </body>
